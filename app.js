54e7b0b273091380d949b3d66176e8a3b604f25e
jorgeemzu <jorge.emzu@gmail.com>
2020-07-20 22:41:35 -0430

    added fade in animations using intersection observer

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const nav = document.querySelector('.nav__menu');
 const hambIcon = document.querySelector('.hamburger__menu');
+const fader = document.querySelectorAll('.fade-in');
 
 function displayMenu(e) {
 	nav.classList.toggle('nav__menu--display');
 }
 
 hambIcon.addEventListener('click', displayMenu);
+
+const apearOptions = {
+	rootMargin: '0px 0px -100px 0px',
+};
+
+const apearOnScroll = new IntersectionObserver((entries, apearOnScroll) => {
+	entries.forEach(entrie => {
+		if (!entrie.isIntersecting) {
+			return;
+		} else {
+			entrie.target.classList.add('apear');
+			apearOnScroll.unobserve(entrie.target);
+		}
+	});
+}, apearOptions);
+
+fader.forEach(fader => {
+	apearOnScroll.observe(fader);
+});
