cd2c8daf6a3a724cff58814e112e76dbf0a7c90d
jorgeemzu <jorge.emzu@gmail.com>
2020-07-21 19:35:04 -0430

    Added slider on hero section

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const nav = document.querySelector('.nav__menu');
 const hambIcon = document.querySelector('.hamburger__menu');
 const fader = document.querySelectorAll('.fade-in');
-
+const heroImge = document.querySelector('.hero__img img');
+const btnNext = document.querySelector('.btn__next');
+const btnPrev = document.querySelector('.btn__prev');
+const heroText = document.querySelector('.hero__text');
+const heroImgs = [
+	{
+		img: 'images/hero-img-family-min.jpg',
+		texto:
+			'<h1>La vida <span>no para</span> mientras estas haciendo planes</h1> <p>No esperes más para <span class="hero__span--green">Asegurarte</span></p> <a class="btn__green" href="#">Obten tu póliza</a>',
+	},
+	{
+		img: 'images/hero-img-car-min.jpg',
+		texto:
+			'<h1>No esperes más <span>para Asegurar</span> tu auto</h1> <p>Es momento de proteger <span class="hero__span--green">Tu Inversion</span></p> <a class="btn__green" href="#">Obten tu póliza</a>',
+	},
+	{
+		img: 'images/hero-img-house-min.jpg',
+		texto:
+			'<h1>prueba <span>para Asegurar</span> tu auto</h1> <p>Es momento de proteger <span class="hero__span--green">Tu Inversion</span></p> <a class="btn__green" href="#">Obten tu póliza</a>',
+	},
+];
+let index = 0;
 function displayMenu(e) {
 	nav.classList.toggle('nav__menu--display');
 }
@@ -26,3 +47,40 @@ const apearOnScroll = new IntersectionObserver((entries, apearOnScroll) => {
 fader.forEach(fader => {
 	apearOnScroll.observe(fader);
 });
+
+function nextImg() {
+	if (index === heroImgs.length) {
+		index = 0;
+	}
+	heroImge.src = `${heroImgs[index].img}`;
+	heroText.innerHTML = `${heroImgs[index].texto}`;
+
+	index++;
+	setTimeout(nextImg, 5000);
+	console.log(heroImge.style.opacity);
+}
+
+nextImg();
+
+// function prevImg() {
+// 	heroImge.src = `${heroImgs[index - 1].img}`;
+// 	heroText.innerHTML = `${heroImgs[index - 1].texto}`;
+// 	index--;
+// }
+
+// btnNext.addEventListener('click', e => {
+// 	e.preventDefault();
+// 	if (index === heroImgs.length - 1) {
+// 		index = -1;
+// 	}
+
+// 	nextImg();
+// });
+// btnPrev.addEventListener('click', e => {
+// 	e.preventDefault();
+// 	if (index === 0) {
+// 		index = heroImgs.length;
+// 	}
+
+// 	prevImg();
+// });
